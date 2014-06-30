0cce80cb53457760f815934408f90cf1784bb802
Xander <xander@Xanders-MacBook-Air.local>
2014-06-30 17:14:28 -0400

    Added Flask App
    Added app that uses flask to open mako file, which contains form for
    ZNC sign-up.

---
+++ app.py
@@ -0,0 +1,27 @@
+import flask
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+mako_lookup = TemplateLookup(directories=['templates'])
+
+
+def render(tpl, **kwargs):
+    return mako_lookup.get_template(tpl).render(**kwargs)
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return '<a href="/register">Register</a>'
+
+
+@app.route('/register')
+def register():
+
+    return render('register.mako', title="done")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
