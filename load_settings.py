b1441f993702762765e7f8e8a5e769bf04d66c1a
Kurtis Jungersen <kmjungersen@gmail.com>
2014-07-14 16:56:20 -0400

    completely changed the way settings are loaded accross the app

--- load_settings.py
+++ load_settings.py
@@ -9,23 +9,39 @@ class LocalSettings():
 
         self.znc_ip = settings['znc_ip_address']
         self.znc_port = int(settings['znc_port_number'])
+
         self.znc_username = settings['znc_username']
         self.znc_password = settings['znc_password']
 
+        self.username_chars = settings['username_characters']
+        self.password_chars = settings['password_characters']
+
         self.register_port = int(settings['registration_port_number'])
         self.channel = settings['default_channel']
+        self.contact_email = settings['contact_email']
+
+        self.client_enabled = bool(settings['custom_irc_client_enabled'])
+        self.client_ip = settings['client_ip_address']
+        self.client_port = int(settings['client_port'])
 
     def load_settings(self, config_file_path):
+
+        config_settings = {}
 
         config = RawConfigParser()
         config.read(config_file_path)
 
-        section = 'ZNC CONFIGURATION OPTIONS'
-        options_list = config.options(section)
+        section_list = ['ZNC CONFIGURATION OPTIONS',
+                        'WEB REGISTRATION CONSOLE OPTIONS',
+                        'IRC CLIENT OPTIONS',
+                        ]
 
-        config_settings = {}
+        for section in section_list:
 
-        for option in options_list:
-            config_settings[option] = config.get(section, option)
+            options_list = config.options(section)
+
+            for option in options_list:
+
+                config_settings[option] = config.get(section, option)
 
         return config_settings
